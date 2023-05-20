b0ab1e039adb8132a5ce7d924d602a50865c6553
Arfan Rahman Yudiantoro <arfanrahman@student.telkomuniversity.ac.id>
2023-05-20 17:35:27 +0700

    6.membuat optionsmenu dan history

--- app/src/main/java/com/arfan/diaryku/ui/NoteFragment.kt
+++ app/src/main/java/com/arfan/diaryku/ui/NoteFragment.kt
@@ -5,10 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.arfan.diaryku.databinding.FragmentHistoriBinding
+import com.arfan.diaryku.databinding.FragmentNoteBinding
 
-class HistoriFragment: Fragment() {
-    private lateinit var binding: FragmentHistoriBinding
+class NoteFragment: Fragment() {
+    private lateinit var binding: FragmentNoteBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
--- app/src/main/java/com/arfan/diaryku/ui/TambahFragment.kt
+++ app/src/main/java/com/arfan/diaryku/ui/TambahFragment.kt
@@ -2,10 +2,12 @@ package com.arfan.diaryku.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.arfan.diaryku.R
 import com.arfan.diaryku.databinding.FragmentTambahBinding
 
@@ -22,13 +24,17 @@ class TambahFragment: Fragment() {
         return binding.root
     }
 
-//    private fun updateUI(fragment: TambahFragment){
-//        binding = FragmentTambahBinding.inflate(layoutInflater)
-//
-//    }
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-       // updateUI(fragment = TambahFragment())
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.menu_histori -> {
+                findNavController().navigate(R.id.action_tambahFragment_to_historiFragment)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
