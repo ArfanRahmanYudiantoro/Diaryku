f77a81837084782c13b6fba0f6d758bc6700f1b5
Arfan Rahman Yudiantoro <arfanrahman@student.telkomuniversity.ac.id>
2023-05-22 15:56:36 +0700

    17.test

--- app/src/main/java/com/arfan/diaryku/db/DataDao.kt
+++ app/src/main/java/com/arfan/diaryku/db/DataDao.kt
@@ -13,13 +13,10 @@ interface DataDao {
     @Query("SELECT * FROM data ORDER BY id DESC")
     fun getData(): LiveData<List<DataEntity>>
 
-    @Query("SELECT * FROM data ORDER BY id DESC")
-    fun getLastHistory(): LiveData<List<DataEntity>>
 
     @Query("SELECT * FROM data ORDER BY id DESC LIMIT 1")
     fun getLastHistoryData(): LiveData<DataEntity>
-    @Query("DELETE FROM data")
-    fun deleteAllHistory()
+
 
     @Query("DELETE FROM data WHERE id = :id")
     fun deleteHistory(id: Long)
